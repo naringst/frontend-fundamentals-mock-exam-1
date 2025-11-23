d1798050ec1edaa9e4273b77d542d05b9ec14c56
Nari Jeong <92130993+naringst@users.noreply.github.com>
2025-11-23 22:32:48 +0900

    refactor: ErrorBoundary는 페이지 전체 범위로 수정

--- src/pages/savingsCalculator/index.tsx
+++ src/pages/savingsCalculator/index.tsx
@@ -7,6 +7,14 @@ import { SavingsProducts } from './components/SavingsProducts';
 import { CalculateResult } from './components/CalculateResult';
 
 export function SavingsCalculatorPage() {
+  return (
+    <ErrorBoundary fallback={<div>오류가 발생했습니다.</div>}>
+      <SavingsCalculator />
+    </ErrorBoundary>
+  );
+}
+
+export function SavingsCalculator() {
   const [goalPrice, setGoalPrice] = useState<number>(0);
   const [monthlyAmount, setMonthlyAmount] = useState<number>(0);
   const [term, setTerm] = useState<number>(12);
@@ -51,28 +59,26 @@ export function SavingsCalculatorPage() {
         </Tab.Item>
       </Tab>
 
-      <ErrorBoundary fallback={<div>오류가 발생했습니다.</div>}>
-        <Suspense fallback={<div>로딩 중...</div>}>
-          {(() => {
-            switch (currentTab) {
-              case 'products':
-                return (
-                  <SavingsProducts
-                    filter={{ goalPrice, monthlyAmount, term }}
-                    selectedProductId={selectedProductId}
-                    onProductSelect={handleProductSelect}
-                  />
-                );
-              case 'results':
-                return (
-                  <CalculateResult filter={{ goalPrice, monthlyAmount, term }} selectedProductId={selectedProductId} />
-                );
-              default:
-                return null;
-            }
-          })()}
-        </Suspense>
-      </ErrorBoundary>
+      <Suspense fallback={<div>로딩 중...</div>}>
+        {(() => {
+          switch (currentTab) {
+            case 'products':
+              return (
+                <SavingsProducts
+                  filter={{ goalPrice, monthlyAmount, term }}
+                  selectedProductId={selectedProductId}
+                  onProductSelect={handleProductSelect}
+                />
+              );
+            case 'results':
+              return (
+                <CalculateResult filter={{ goalPrice, monthlyAmount, term }} selectedProductId={selectedProductId} />
+              );
+            default:
+              return null;
+          }
+        })()}
+      </Suspense>
     </>
   );
 }
